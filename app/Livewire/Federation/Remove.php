aea9ec65059a88f98d8676d20662988e8d51a44a
mrai64 <maxrainato@libero.it>
2025-08-25 13:22:17 +0200

    chore: dev diary update

---
+++ app/Livewire/Federation/Remove.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Federation;
+
+use Livewire\Component;
+
+class Remove extends Component
+{
+    public function render()
+    {
+        return view('livewire.federation.remove');
+    }
+}
