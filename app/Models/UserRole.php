4b778968c8bf0eeae94265c9009690eedde567f2
mrai64 <maxrainato@libero.it>
2025-09-19 18:56:17 +0200

    feat: create UserRoles

---
+++ app/Models/UserRole.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * UserRoles is child of Users
+ * 
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UserRole extends Model
+{
+    use HasFactory, SoftDeletes;
+    //
+    public const table_name = 'user_roles';
+    public const array valid_roles = [
+        'chairman of',
+        'member of',
+        'president of',
+        'secretary of',
+    ];
+
+    // no factory and seeders, not now
+    protected $fillable = [
+        // 'id',
+        'user_id', // a uuid from users.id
+        'role', // one of valid_roles[]
+        'organization_id', // a uuid from organizations.id | NULL 
+        'contest_id', // a uuid from contests.id | NULL
+        'federation_id', // a uuid from federations.id | NULL
+        'role_opening', // datetime
+        'role_closing', // datetime >= role_opening
+    ];
+
+    protected function casts()
+    {
+        return [
+            'role_opening' => 'datetime',
+            'role_closing' => 'datetime',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+            'deleted_at' => 'datetime',
+        ];
+    }
+
+    /**
+     * complex validation 
+     */
+    public function only_one_roled(UserRole $user_role) : bool 
+    {
+        // 2 is too much
+        if (($user_role->organization_id) && ($user_role->contest_id)) {
+            return false;
+        }
+        if (($user_role->organization_id) && ($user_role->federation_id)) {
+            return false;
+        }
+        if (($user_role->contest_id) && ($user_role->federation_id)) {
+            return false;
+        }
+        // 1 is right
+        if (($user_role->organization_id) || ($user_role->contest_id) || ($user_role->federation_id)) {
+            return true;
+        }
+        // 0 is too less
+        return false;
+    }
+    /**
+     * validation like enum
+     */
+    public function is_a_valid_role(UserRole $user_role) : bool {
+        return (in_array( $user_role->role, self::valid_roles, true));
+    }
+}
--- database/migrations/2025_09_19_170924_create_user_roles_table.php
+++ database/migrations/2025_09_19_170924_create_user_roles_table.php
@@ -1,23 +1,5 @@
 <?php
-/**
- * User Roles table
- * memorize if a user had a role in organization and/or Federation
- * and which is the role
- * 
- * TODO Factory yes but
- * TODO Seeder 
- * TODO role list
- * - organization/member
- *   Qualified to manage contest
- * - federation/member
- *   Qualified to check contests under her/him federation patronage
- * - Backup n Softdelete yes but also 
- * - begin and closing dateTime for role
- *   warning should be more than a record for same triplet
- *   (user, role, federation) but interval intersection must be void
- * 
- * 
- */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -30,24 +12,32 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_roles', function (Blueprint $table) {
-            $table->id(); // bigint ok 
-            $table->string('user_id',36)->index()
-                ->comment('fk: users.id');
-            $table->string('organization_id',36)->index()
-                ->comment('fk: organizations.id');
+            $table->id(); // bigint ok
+            $table->ForeignUuid('user_id')->index()
+            ->comment('fk: users.id');
             $table->string('role');
-            $table->dateTime('role_opening')->useCurrent()
+            //
+            $table->foreignUuid('organization_id')->nullable()->index()
+                ->comment('fk: organizations.id');
+            $table->foreignUuid('contest_id')->nullable()->index()
+                ->comment('fk: contests.id');
+            $table->foreignUuid('federation_id')->nullable()->index()
+                ->comment('fk: federations.id');
+            //
+            $table->dateTime('role_opening')->useCurrent()->index()
                 ->comment('Start of role works - default today');
             $table->dateTime('role_closing')->default('9999-12-31 23:59:59')
                 ->comment('End of role works default:future');
-            // 
+            //
             // backup and softdelete - don't need Tz anymore
             $table->dateTime('created_at')->useCurrent()
                 ->comment('backup reserved');
             $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate()
                 ->comment('backup reserved');
             $table->dateTime('deleted_at')->nullable()
-                ->comment('softdelete reserved'); 
+                ->comment('softdelete reserved');
+            //
+            $table->index(['user_id','organization_id','contest_id','federation_id','role_opening','id'], 'general_idx');
         });
     }
 
