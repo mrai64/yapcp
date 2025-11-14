a2f59fea9a46b8fc6776ab0c62ad1d0770a9f463
mrai64 <maxrainato@libero.it>
2025-11-14 21:35:54 +0100

    reorder seeders

--- database/seeders/e_ContestChargerSeeder.php
+++ database/seeders/e_ContestChargerSeeder.php
@@ -40,7 +40,7 @@ class ContestChargerSeeder extends Seeder
     public $user_in_user_role;
     public $user_in_contest;
     public $excluded_ids;
-    public $id;
+    public $work;
     public $work_participant;
     public $added_work;
     public $copy_result;
@@ -59,7 +59,7 @@ class ContestChargerSeeder extends Seeder
         $this->opened_contest = Contest::where('day_3_jury_opening', '<=', $this->today)->where('day_4_jury_closing', '>=', $this->today )->inRandomOrder()->first();
         Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' contest:'. json_encode($this->opened_contest) );
 
-        echo "{$this->opened_contest->name_en} \n";
+        echo "\n"."{$this->opened_contest->name_en} \n";
 
         // 2. all section
         $this->contest_section_list = ContestSection::where('contest_id', $this->opened_contest->id)->get();
@@ -68,70 +68,93 @@ class ContestChargerSeeder extends Seeder
         // 4. user_id in user_roles
         $this->user_in_user_role = DB::table( UserRole::table_name)->get('user_id');
         Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' user_role_in:'. json_encode( $this->user_in_user_role) );
+
+        // from collection to array - old way
         $this->excluded_ids = [];
         foreach($this->user_in_user_role as $key => $user_id){
             $this->excluded_ids[]=$user_id;
         }
+        // 4.b already in 
         $this->user_in_contest = ContestParticipant::select('user_id')->where('contest_id', $this->opened_contest->id)->get();
         Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' already_in:'. ($this->user_in_contest) );
+        // from collection to array - old way
         foreach($this->user_in_user_role as $key => $user_id){
             $this->excluded_ids[] = $user_id;
         }
         // Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' exclude_list:'. json_encode($this->excluded_ids) );
         
         // infinite loop risk is not zero dot zero periodic
-        // i j k index
+        // i j k index more clear ind_i ind_j ind_k
         for ($i=0; $i < 24 ; ) { 
-            # code...
-            $this->work_participant = DB::table( UserContact::table_name)
-                ->select('user_contacts.*')
-                ->orderBy(DB::raw('RAND()'))
-                ->take(5)
-                ->first();
+
+            // Random user
+            // $this->work_participant = DB::table( UserContact::table_name)
+            //     ->select('user_contacts.*')
+            //     ->orderBy(DB::raw('RAND()'))
+            //     ->take(5)
+            //     ->first();
+            $this->work_participant = UserContact::whereNull('deleted_at')->inRandomOrder()->first();
             Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' candidate:'. json_encode($this->work_participant) );
-            // out if already in or had any (too large) role as juror, organization etc.
-            // right exclusion only if role is in organization and juror in contest
+
+            // excluded_ids is user already in + user in user_roles 
             if ( in_array( $this->work_participant->user_id, $this->excluded_ids)) {
                 Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' out:'. json_encode($this->work_participant->user_id) );
                 continue;
             }
+
+            // new, he's /she's must be added
+            $this->user_participant = ContestParticipant::create([
+                'contest_id' => $this->opened_contest->id,
+                'user_id'    => $this->work_participant->user_id,
+                'fee_payment_completed' => 'Y',
+            ]);
             Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' new work_participant:'. json_encode($this->work_participant) );
+
             $i++;
-            echo "\n ". sprintf('%d03', $i);
+            echo "\n ". sprintf('%03d', $i);
 
             foreach ($this->contest_section_list as $this->contest_section) {
                 Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' Section:'. ($this->contest_section->name_en) );
                 echo " ".$this->contest_section->name_en . " ";
-                for ($j=0; $j < $this->contest_section->rule_max; $j++) { 
-                    $this->id = DB::table(Work::table_name)
-                        ->select(Work::table_name.'.*')->orderBy(DB::raw('RAND()'))
-                        ->take(5)->first();
-                    Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' work_id:'. json_encode($this->id) );
+
+                for ($j=1; $j <= $this->contest_section->rule_max; ) { 
+
+                    // picked casually from works table 
+                    // $this->work = DB::table(Work::table_name)
+                    //     ->select(Work::table_name.'.*')
+                    //     ->orderBy(DB::raw('RAND()'))
+                    //     ->take(5)->first();
+                    $this->work = Work::whereNull('deleted_at')->inRandomOrder()->first();
+                    Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' work_id:'. json_encode($this->work) );
+
+                    $present = ContestWork::where('contest_id', $this->opened_contest->id)
+                        ->where('section_id', $this->contest_section->id)
+                        ->where('work_id', $this->work->id)
+                        ->count();
+
+                    if ($present){
+                        echo ' DUP:' . $this->work->id;
+                        continue;
+                    }
+
                     $this->added_work = ContestWork::create([
                         'contest_id' => $this->opened_contest->id,
                         'section_id' => $this->contest_section->id,
                         'country_id' => $this->work_participant->country_id,
                         'user_id'    => $this->work_participant->user_id,
-                        'work_id'    => $this->id->id,
-                        'portfolio_sequence' => ($j + 1),
+                        'work_id'    => $this->work->id,
+                        'portfolio_sequence' => ($j),
                     ]);
+                    $j++;
                     Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' new work_participant:'. json_encode($this->added_work) );
 
-                    $this->file_to = 'contests/' . $this->opened_contest->id .'/'. $this->contest_section->id .'/'. $this->id->id .'.'. $this->id->extension;
-                    $this->copy_result = Storage::disk('public')->copy( 'photos/'.$this->id->work_file, $this->file_to);
-                    echo ($this->copy_result) ? '✅ ' : '🥸 ';
+                    $this->file_to = 'contests/' . $this->opened_contest->id .'/'. $this->contest_section->id .'/'. $this->work->id .'.'. $this->work->extension;
+                    $this->copy_result = Storage::disk('public')->copy( 'photos/'.$this->work->work_file, $this->file_to);
+                    echo ($this->copy_result) ? '✅ ' : "\n".'🥸 from:'. $this->work->work_file .' to:'. $this->file_to."\n";
 
                 } // for j
             } // foreach
 
-            // 7. add user contest participant 
-            if ( ContestParticipant::where('contest_id', $this->opened_contest->id )->where('user_id', $this->work_participant->user_id )->count() === 0) {
-                $this->user_participant = ContestParticipant::create([
-                    'contest_id' => $this->opened_contest->id,
-                    'user_id'    => $this->work_participant->user_id,
-                    'fee_payment_completed' => 'Y',
-                ]);
-            }
         } //  for i
         echo "\n";
     } // run
--- database/seeders/e_ContestVoteSeeder.php
+++ database/seeders/e_ContestVoteSeeder.php
@@ -22,54 +22,70 @@ class ContestVoteSeeder extends Seeder
         echo "\n" . __CLASS__;
         echo "\n" . "Find contest";
         $contests_set =  Contest::all();
+        echo "\n". '§ contest #'. $contests_set->count();
+
         foreach($contests_set as $contest){
-            
+
             echo "\n***********************************************************************************************************";
             echo "\n*** Contest:" . $contest->name_en;
             echo "\n***********************************************************************************************************";
-            
+
             $sections_set = ContestSection::where('contest_id', $contest->id)->get();
-            
+            echo "\n". '§ Section #:' . $sections_set->count(); 
+
             foreach($sections_set as $section) {
                 echo "\n\n***********************************************************************************************************";
                 echo "\nSection:" . $section->name_en;
                 echo "\n***********************************************************************************************************";
-                
+
                 $jurors_set = ContestJury::where('section_id', $section->id)->get();
+                echo "\n". '§ Jury members #: ' . $jurors_set->count();
                 
-                $contest_works_set = ContestWork::where('section_id', $section->id)->where('contest_id', $contest->id)->get();
+                $contest_works_set = ContestWork::where('contest_id', $contest->id )->where('section_id', $section->id )->get();
+                echo "\n". '§ section_id:'.$section->id.' Work_set #: ' . $contest_works_set->count();
                 
                 foreach($contest_works_set as $contest_work){
-                    
+
                     foreach($jurors_set as $juror){
-                        
-                        $already_voted = ContestVote::where('section_id', $section->id)->where('contest_id', $contest->id)->where('work_id', $contest_work->id)->where('juror_user_id', $juror->id)->count();
-                        
-                        if ($already_voted){
+
+                        $already_voted = ContestVote::where('section_id', $section->id)
+                            ->where('work_id',       $contest_work->work_id)
+                            ->where('contest_id',    $contest->id)
+                            ->where('juror_user_id', $juror->id)->count();
+
+                        if ($already_voted) {
+                            // $contest_work->delete();
+                            // echo ' DUP DEL';
                             echo ' .';
                             continue;
                         }
-                        
-                        $vote = rand(2,6)+rand(2,6)+rand(2,6)+rand(2,6)+rand(2,6)+rand(0,6);
+                        // assuming $contest->vote_rule == 'num:1..10'
+                        // $vote = rand(2,4)+rand(2,4)+rand(0,5)
+                        // if ($vote > 10) {
+                        //     $vote = 10;
+                        // }
+
+                        // assuming $contest->vote_rule == 'num:1..30' 
+                        $vote = rand(3,6)+rand(3,6)+rand(3,6)+rand(3,6)+rand(0,6)+rand(0,6);
                         if ($vote > 30) {
                             $vote = 30;
                         }
-                        
+
                         $inserted  = ContestVote::create([
                             'contest_id'    => $contest->id,
                             'section_id'    => $section->id,
-                            'work_id'       => $contest_work->id,
+                            'work_id'       => $contest_work->work_id,
                             'juror_user_id' => $juror->id,
                             'vote'          => $vote,
                         ]);
-                        
+
                         echo ("\n".'Seeder '. __CLASS__ .' inserted '. $inserted->id . ' vote:'. $vote .' rec:'. json_encode($inserted) );
-                        
+
                     } // $jurors_set
                 } // contest_work_set
                 echo "\n\n***********************************************************************************************************";
                 echo "\n*** Section change ****************************************************************************************";
-                echo "\n***********************************************************************************************************";
+                echo "\n***********************************************************************************************************\n";
             } // sections_set
         } // contests_set
     }
