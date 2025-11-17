c04086bc92cd2d1dceba927f4e6b0d8332485bd0
mrai64 <maxrainato@libero.it>
2025-11-17 08:15:23 +0100

    cohore dev diary update

--- database/seeders/d_ParticipantSeeder.php
+++ database/seeders/d_ParticipantSeeder.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
-class ParticipantSeeder extends Seeder
+class d_ParticipantSeeder extends Seeder
 {
     public $user;
     public $country = [
@@ -56,12 +56,14 @@ class ParticipantSeeder extends Seeder
      */
     public function run(): void
     {
+        echo "\n". __CLASS__;
         $this->user = User::factory()->create();
         Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' user:' . json_encode($this->user) );
         [$this->last_name, $this->first_name] = explode(', ', $this->user->name );
+        echo "\n". $this->last_name .' '. $this->first_name;
         $this->photo_list = Storage::disk('public')->files('photos/2500px');
         Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' user:' . json_encode($this->photo_list) );
-
+        
         $this->user_contact = UserContact::where('user_id', $this->user->id)->first();
         $this->user_contact->update([
             'country_id' => $this->country[( rand(1,count($this->country)) - 1)],
@@ -76,10 +78,10 @@ class ParticipantSeeder extends Seeder
             'whatsapp'   => '',
         ]);
         Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' user:' . json_encode($this->user_contact) );
-        Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' user:' . json_encode($this->user_contact) );
-
-        for ($i=0; $i < 12; $i++) {
-
+        echo ("\n".'Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' user:' . json_encode($this->user_contact) );
+        
+        for ($i=0; $i < 48; $i++) {
+            
             $this->work = Work::factory()->create([
                 'user_id'    => $this->user_contact->user_id,
                 'work_file'  => '',
@@ -89,26 +91,30 @@ class ParticipantSeeder extends Seeder
             ]);
             Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' work:' . json_encode($this->work) );
             $this->work_id = $this->work->id;
-
+            
             $this->file_to = 'photos/'. $this->user_contact->country_id .'/'. $this->user_contact->last_name
             .'/'. $this->user_contact->first_name .'_'. $this->user_contact->user_id
             .'/'. $this->work_id .'.jpg';
             $this->file_to = str_ireplace(' ', '_', $this->file_to);
             Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' ..to:' . json_encode($this->file_to) );
-
+            
             $this->file_from = $this->photo_list[( rand(1,count($this->photo_list)) - 1)];
             Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' from:' . json_encode($this->file_from) );
             // Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' from?:' . (Storage::disk('public')->fileExists($this->file_from) ?"✅":"🟥") );
-
+            
             // $this->copy_result = Storage::copy( $this->file_from, $this->file_to );
             $this->copy_result = Storage::disk('public')->copy( $this->file_from, $this->file_to );
             Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' copy res:' . ($this->copy_result ?"✅":"🟥") );
-
+            
             $this->work->update([
                 'work_file' => str_ireplace('photos/', '', $this->file_to )
             ]);
             $this->work->save();
             Log::info('Seeder '. __CLASS__ .' f:'. __FUNCTION__.' l:'. __LINE__ . ' work:' . json_encode($this->work) );
+            echo ("\n\n".($i + 1).".\n".' work:' . json_encode($this->work) );
         }
+        echo "\n";
+        echo "\nFor user: ". $this->last_name .' '. $this->first_name;
+        echo "\n";
     }
 }
