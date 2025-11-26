c8910f810631d3bbfdfae750de7d97da7ef65f02
mrai64 <maxrainato@libero.it>
2025-11-26 17:35:25 +0100

    feat: policies add

---
+++ app/Policies/JurorOnlyPolicy.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Restrict access to jury member 
+ * 
+ */
+namespace App\Policies;
+
+use App\Models\ContestJury;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+class JurorOnlyPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        Log::info('Policies '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' called');
+    }
+    
+    /**
+     * @param string $section_id
+     * 
+     */
+    public function grant_access(string $section_id) : Response
+    {
+    Log::info('Policies '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' called');
+    $user_id = Auth::id();
+    $access_granted = ContestJury::where('section_id', $section_id)->where('user_contact_id', $user_id)->count(); // 1 | 0
+    Log::info('Policies '. __CLASS__ .' f:'. __FUNCTION__ .' l:'. __LINE__ .' res:' . $access_granted . ' for user:'. $user_id);
+    if ($access_granted > 0){
+        return Response::allow();
+    }
+
+    return Response::deny( __("You can't") );
+    }
+
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * 2025-10-11 Add first gates 'contest-participants-update'
+ * 2025-11-26 Add 'juror-only' gate
  * 
  */
 namespace App\Providers;
 
 use App\Policies\ContestPaymentChangePolicy;
+use App\Policies\JurorOnlyPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,5 +28,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Gate::define('contest-participants-update', [ContestPaymentChangePolicy::class, 'update']);
+        Gate::define('jury-panels', [JurorOnlyPolicy::class, 'grant_access']);
     }
 }
