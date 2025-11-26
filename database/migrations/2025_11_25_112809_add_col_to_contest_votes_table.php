65a7fac3541b8bfb57c5b5c539d811178ef0cf6c
mrai64 <maxrainato@libero.it>
2025-11-26 17:33:04 +0100

    feat: vote review

---
+++ database/migrations/2025_11_25_112809_add_col_to_contest_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Add review col to contest_votes_table 
+ * 
+ * 
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contest_votes', function (Blueprint $table) {
+            $table->boolean('review_required')->default(false)
+            ->after('vote')->comment("0 = not required");
+            // idx
+            $table->index(['section_id','juror_user_id','review_required','work_id'], 'review_idx');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contest_votes', function (Blueprint $table) {
+            $table->dropIndex('review_idx');
+            $table->dropColumn('review_required');
+        });
+    }
+};
