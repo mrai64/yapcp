f0799e43b38865d071ff430e48324e1e16a11074
mrai64 <maxrainato@libero.it>
2026-02-10 16:17:24 +0100

    refactor: migration / 12

---
+++ app/Http/Controllers/UserWorkValidationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserWorkValidationRequest;
+use App\Http\Requests\UpdateUserWorkValidationRequest;
+use App\Models\UserWorkValidation;
+
+class UserWorkValidationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserWorkValidationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserWorkValidation $userWorkValidation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserWorkValidation $userWorkValidation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserWorkValidationRequest $request, UserWorkValidation $userWorkValidation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserWorkValidation $userWorkValidation)
+    {
+        //
+    }
+}
---
+++ database/migrations/2026_02_05_000010_create_user_work_validations_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_work_validations', function (Blueprint $table) {
+            $table->id()->comment('real pk is: work_id + federation_section_id'); // no uuid
+            $table->char('work_id', 36)->charset('ascii')->collation('ascii_general_ci')
+                ->index()->comment('fk: user_works.id');
+            $table->unsignedBigInteger('federation_section_id')->index()
+                ->comment('fk: federation_sections.id ');
+            $table->char('validator_user_id', 36)->charset('ascii')->collation('ascii_general_ci')
+                ->index()->comment('contest organization members that validate the work for specific section');
+            //
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrentOnUpdate()->useCurrent()->index();
+            $table->dateTime('deleted_at')->nullable()->index();
+            //
+            $table->unique(['work_id', 'federation_section_id', 'deleted_at'], 'general_idx');
+            //
+            $table->foreign(['work_id'])->references(['id'])->on('user_works')
+                ->onUpdate('restrict')->onDelete('restrict');
+            $table->foreign(['validator_user_id'])->references(['id'])->on('user_contacts')
+                ->onUpdate('restrict')->onDelete('restrict');
+            $table->foreign(['federation_section_id'])->references(['id'])->on('federation_sections')
+                ->onUpdate('restrict')->onDelete('restrict');
+            //
+            $table->comment('human check for user_works on federation_sections');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_work_validations');
+    }
+};
