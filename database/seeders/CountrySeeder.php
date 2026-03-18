a0f109f810877c4441a915e723d2ae7d012f4eca
mrai64 <maxrainato@libero.it>
2026-03-18 10:10:21 +0100

    feat: CountrySeeder use local file

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -15,9 +15,6 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class CountrySeeder extends Seeder
