37cb2fd1b26f0a6164073496b15c6d4e53875626
mrai64 <maxrainato@libero.it>
2026-03-18 10:10:21 +0100

    feat: CountrySeeder use local file

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -12,33 +12,70 @@ namespace Database\Seeders;
 
 use App\Models\Country;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class CountrySeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * Use a local file or
+     * Dload external source from https://github.com/mledoze/countries
+     *
      */
     public function run()
     {
-        echo "Give reference json from github";
+        $this->command->info(__CLASS__ . "...");
+        $filePath = 'private/countries.json';
+        $remoteUrl = 'https://raw.githubusercontent.com/mledoze/countries/master/countries.json';
 
-        // Dload external source from https://github.com/mledoze/countries
-        $json = file_get_contents('https://raw.githubusercontent.com/mledoze/countries/master/countries.json');
+        // check local fle
+        if (!Storage::disk('local')->exists($filePath)){
+            $this->command->info("Missing local file - Give reference json from github");
+            try {
+                // pick
+                $response = Http::get($remoteUrl);
+                if ($response->successful()){
+                    Storage::disk('local')->put($filePath, $response->body());
+                    $this->command->info("Saved local");
+                } else {
+                    throw new \Exception("Not saved local file - Countries reference json from github status:" . $response->status());
+                }
+
+            } catch (\Throwable $th) {
+                //throw $th;
+                Log::error("Error in CountrySeeder picking remote json file with: " . $th->getMessage());
+                $this->command->error("Blocked bu error: " . $th->getMessage());
+                return;
+            }
+
+        }
+
+        $json = Storage::disk('local')->get($filePath);
         $countries = json_decode($json, true);
+        if (is_array($countries)) {
+            $this->command->getOutput()->progressStart(count($countries));
 
-        foreach ($countries as $c) {
-            Country::updateOrCreate(
-                ['id' => $c['cca3']], // country code alpha 3 >> iso-3166 alpha-3
-                [
-                    'country'      => $c['name']['common'],
-                    'flag_code'    => $c['flag'] ?? '',
-                    'lang_code'    => Str::limit(array_key_first($c['languages'] ?? ['en' => '']), 2, ''),
-                    'locale'       => Str::limit(array_key_first($c['languages'] ?? ['en' => '']), 2, '')
-                        . '_' . $c['cca2'],
-                    'calling_code' => ($c['idd']['root'] ?? '') . ($c['idd']['suffixes'][0] ?? ''),
-                ]
-            );
+            foreach ($countries as $c) {
+                Country::updateOrCreate(
+                    ['id' => $c['cca3']], // country code alpha 3 >> iso-3166 alpha-3
+                    [
+                        'country'      => $c['name']['common'],
+                        'flag_code'    => $c['flag'] ?? '',
+                        'lang_code'    => Str::limit(array_key_first($c['languages'] ?? ['en' => '']), 2, ''),
+                        'locale'       => Str::limit(array_key_first($c['languages'] ?? ['en' => '']), 2, '')
+                            . '_' . $c['cca2'],
+                        'calling_code' => ($c['idd']['root'] ?? '') . ($c['idd']['suffixes'][0] ?? ''),
+                    ]
+                );
+                $this->command->getOutput()->progressAdvance();
+            }
+
+            $this->command->getOutput()->progressFinish();
+            $this->command->info("Done");
         }
     }
 }
