562e80d04a326a18be114eb3ff4efadbd338e891
mrai64 <maxrainato@libero.it>
2026-03-18 10:10:21 +0100

    feat: CountrySeeder use local file

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -15,19 +15,12 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class CountrySeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * Use a local file or
-     * Dload external source from https://github.com/mledoze/countries
-     *
      *
      * Use a local file or
      * Dload external source from https://github.com/mledoze/countries
@@ -51,38 +44,21 @@ class CountrySeeder extends Seeder
                 } else {
                     throw new \Exception("Not saved local file - Countries reference json from github status:" . $response->status());
                 }
+
             } catch (\Throwable $th) {
                 //throw $th;
                 Log::error("Error in CountrySeeder picking remote json file with: " . $th->getMessage());
                 $this->command->error("Blocked bu error: " . $th->getMessage());
                 return;
             }
+
         }
 
         $json = Storage::disk('local')->get($filePath);
         $countries = json_decode($json, true);
         if (is_array($countries)) {
             $this->command->getOutput()->progressStart(count($countries));
-        if (is_array($countries)) {
-            $this->command->getOutput()->progressStart(count($countries));
 
-            foreach ($countries as $c) {
-                Country::updateOrCreate(
-                    ['id' => $c['cca3']], // country code alpha 3 >> iso-3166 alpha-3
-                    [
-                        'country'      => $c['name']['common'],
-                        'flag_code'    => $c['flag'] ?? '',
-                        'lang_code'    => Str::limit(array_key_first($c['languages'] ?? ['en' => '']), 2, ''),
-                        'locale'       => Str::limit(array_key_first($c['languages'] ?? ['en' => '']), 2, '')
-                            . '_' . $c['cca2'],
-                        'calling_code' => ($c['idd']['root'] ?? '') . ($c['idd']['suffixes'][0] ?? ''),
-                    ]
-                );
-                $this->command->getOutput()->progressAdvance();
-            }
-
-            $this->command->getOutput()->progressFinish();
-            $this->command->info("Done");
             foreach ($countries as $c) {
                 Country::updateOrCreate(
                     ['id' => $c['cca3']], // country code alpha 3 >> iso-3166 alpha-3
