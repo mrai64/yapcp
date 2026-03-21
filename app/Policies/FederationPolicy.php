2a2e9d97b83f4d40d550491e538f491d444de30f
mrai64 <maxrainato@libero.it>
2026-03-21 15:57:48 +0100

    chore: comments [skip ci]

--- app/Policies/FederationPolicy.php
+++ app/Policies/FederationPolicy.php
@@ -103,4 +103,16 @@ class FederationPolicy
         Log::info('Policy: ' . __CLASS__ . ' ' . __FUNCTION__ . ' line:' . __LINE__ . ' evaluated:' . $evaluate);
         return $evaluate;
     }
+
+    /**
+     * Determine whether the user can backup the model
+     */
+    public function backuppable(User $user, Federation $federation) : bool
+    {
+        // for all federation - only for user in admin group
+        ds('Policy: ' . __CLASS__ . ' ' . __FUNCTION__ . ' line:' . __LINE__ . ' called');
+        $evaluate = UserRole::isAdmin();
+        ds('Policy: ' . __CLASS__ . ' ' . __FUNCTION__ . ' line:' . __LINE__ . ' evaluated:' . $evaluate);
+        return $evaluate;
+    }
 }
