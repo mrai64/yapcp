af712831f331f842e7f08211e9ee66d95661e6b7
mrai64 <maxrainato@libero.it>
2026-03-21 15:57:48 +0100

    chore: comments [skip ci]

--- app/Policies/FederationPolicy.php
+++ app/Policies/FederationPolicy.php
@@ -40,6 +40,9 @@ class FederationPolicy
 
     /**
      * Determine whether the user can update the model.
+     *
+     * TODO Modify federation can cause mistake on running contest,
+     * TODO add some check that there is no running contest
      */
     public function update(User $user, Federation $federation): bool
     {
@@ -52,6 +55,9 @@ class FederationPolicy
 
     /**
      * Determine whether the user can delete the model.
+     *
+     * TODO Cannot remove federation with contest sponsored running
+     * TODO check there is no running contest sponsored by federation id.
      */
     public function delete(User $user, Federation $federation): bool
     {
@@ -78,6 +84,18 @@ class FederationPolicy
      * Determine whether the user can permanently delete the model.
      */
     public function forceDelete(User $user, Federation $federation): bool
+    {
+        // for all federation - only for user in admin group
+        ds('Policy: ' . __CLASS__ . ' ' . __FUNCTION__ . ' line:' . __LINE__ . ' called');
+        $evaluate = UserRole::isAdmin();
+        ds('Policy: ' . __CLASS__ . ' ' . __FUNCTION__ . ' line:' . __LINE__ . ' evaluated:' . $evaluate);
+        return $evaluate;
+    }
+
+    /**
+     * Determine whether the user can backup the model
+     */
+    public function backuppable(User $user, Federation $federation) : bool
     {
         // for all federation - only for user in admin group
         ds('Policy: ' . __CLASS__ . ' ' . __FUNCTION__ . ' line:' . __LINE__ . ' called');
