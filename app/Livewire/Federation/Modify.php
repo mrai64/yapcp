1f6bd9e14ab89b186d414e1c0e328b01157e4f90
mrai64 <maxrainato@libero.it>
2026-03-25 14:32:28 +0100

    fix: fix FederationModifyTest

--- app/Livewire/Federation/Modify.php
+++ app/Livewire/Federation/Modify.php
@@ -35,7 +35,7 @@ class Modify extends Component
     public string $countryId;
 
     #[Validate('string')]
-    public string $federationContact;
+    public string $federationContact = '';
 
     // readonly
     public $countries;
@@ -49,8 +49,7 @@ class Modify extends Component
         // $userToCheck = User::find(Auth::id());
         // $this->authorize('update', $userToCheck);
         //
-        Log::info('Component ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
-        Log::info('Component ' . __CLASS__ . ' ' . __FUNCTION__ . ':' . __LINE__ . ' fid' . $federation->id);
+        Log::info('Component ' . __CLASS__ . ' ' . __FUNCTION__ . ':' . __LINE__ . ' called w/fid' . $federation->id);
         $this->federation = $federation; // was: Federation::where('id', $fid)->get()[0];
         Log::info('Component ' . __CLASS__ . ' ' . __FUNCTION__ . ':' . __LINE__ . ' found:' . json_encode($this->federation));
 
