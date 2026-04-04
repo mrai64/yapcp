9c4d2064bba4403c5f9b39158873a49526fea489
mrai64 <maxrainato@libero.it>
2026-04-04 13:38:20 +0200

    feat: federationSection "full set"

---
+++ app/Http/Controllers/FederationSectionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFederationSectionRequest;
+use App\Http\Requests\UpdateFederationSectionRequest;
+use App\Models\FederationSection;
+
+class FederationSectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFederationSectionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FederationSection $federationSection)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FederationSection $federationSection)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFederationSectionRequest $request, FederationSection $federationSection)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FederationSection $federationSection)
+    {
+        //
+    }
+}
