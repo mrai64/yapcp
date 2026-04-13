f191a368071f41204a963d81c19c73539ebc7906
mrai64 <maxrainato@libero.it>
2026-04-13 18:08:36 +0200

    feat: Model ContestSection

---
+++ app/Http/Controllers/ContestSectionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreContestSectionRequest;
+use App\Http\Requests\UpdateContestSectionRequest;
+use App\Models\ContestSection;
+
+class ContestSectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreContestSectionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ContestSection $contestSection)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ContestSection $contestSection)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateContestSectionRequest $request, ContestSection $contestSection)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ContestSection $contestSection)
+    {
+        //
+    }
+}
