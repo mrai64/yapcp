0afd11bbbcaab1844c5e863f4b81135f51b6e4cc
mrai64 <maxrainato@libero.it>
2026-04-20 18:20:19 +0200

    Chore: Pint

--- app/Policies/FederationMorePolicy.php
+++ app/Policies/FederationMorePolicy.php
@@ -4,7 +4,6 @@ namespace App\Policies;
 
 use App\Models\FederationMore;
 use App\Models\User;
-use App\Models\UserRole;
 
 class FederationMorePolicy
 {
--- app/Policies/FederationPolicy.php
+++ app/Policies/FederationPolicy.php
@@ -4,7 +4,6 @@ namespace App\Policies;
 
 use App\Models\Federation;
 use App\Models\User;
-use App\Models\UserRole;
 
 class FederationPolicy
 {
