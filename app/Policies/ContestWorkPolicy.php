364b7d40ae886f5cabc974c5d6e3e6c6526064e5
mrai64 <maxrainato@libero.it>
2026-04-21 11:58:57 +0200

    feat: ContestWork Policies

--- app/Policies/ContestWorkPolicy.php
+++ app/Policies/ContestWorkPolicy.php
@@ -4,24 +4,46 @@ namespace App\Policies;
 
 use App\Models\ContestWork;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class ContestWorkPolicy
 {
     /**
      * Determine whether the user can view any models.
+     *
+     * admin can
      */
     public function viewAny(User $user): bool
     {
-        return false;
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
+        // admin can
+        $evaluate = $user->isAdmin();
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' evaluated:' . $evaluate);
+        // other cannot
+        return $evaluate;
     }
 
     /**
      * Determine whether the user can view the model.
+     *
+     * admin can
+     * user herself / himself can
      */
     public function view(User $user, ContestWork $contestWork): bool
     {
-        return false;
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
+        // user herself / himself can
+        $userContestId = $contestWork?->user_id;
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' id:' . $userContestId);
+        if ($userContestId === $user->id) {
+            return true;
+        }
+        // admin can
+        $evaluate = $user->isAdmin();
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' evaluated:' . $evaluate);
+        // other cannot
+        return $evaluate;
     }
 
     /**
@@ -29,7 +51,11 @@ class ContestWorkPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
+        // user herself / himself can
+        $evaluate = ($user?->id == Auth::id());
+        // other cannot
+        return $evaluate;
     }
 
     /**
@@ -37,7 +63,25 @@ class ContestWorkPolicy
      */
     public function update(User $user, ContestWork $contestWork): bool
     {
-        return false;
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
+        // user herself / himself can
+        $userContestId = $contestWork?->user_id;
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' id:' . $userContestId);
+        if ($userContestId === $user->id) {
+            return true;
+        }
+        // admin can
+        $evaluate = $user->isAdmin();
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' evaluated:' . $evaluate);
+        if ($evaluate) {
+            return true;
+        }
+        // organization members can
+        $organizationId = $contestWork->contest->organization_id;
+        $evaluate = $user->isMemberOfOrganization($organizationId);
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' evaluated:' . $evaluate);
+        // other cannot
+        return $evaluate;
     }
 
     /**
@@ -45,7 +89,14 @@ class ContestWorkPolicy
      */
     public function delete(User $user, ContestWork $contestWork): bool
     {
-        return false;
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
+        // user herself / himself
+        $userContestId = $contestWork?->user_id;
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' id:' . $userContestId);
+        $evaluate = ($userContestId === $user->id);
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' evaluated:' . $evaluate);
+        // other cannot
+        return $evaluate;
     }
 
     /**
@@ -53,6 +104,7 @@ class ContestWorkPolicy
      */
     public function restore(User $user, ContestWork $contestWork): bool
     {
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
         return false;
     }
 
@@ -61,6 +113,7 @@ class ContestWorkPolicy
      */
     public function forceDelete(User $user, ContestWork $contestWork): bool
     {
+        Log::info('Policy: ' . __CLASS__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
         return false;
     }
 }
