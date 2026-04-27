7936684c8a7379c91ca85ae06fc240e2e2218b3e
mrai64 <maxrainato@libero.it>
2026-04-27 15:10:49 +0200

    fix: contest-participant.modify

--- resources/views/livewire/contest/award/add.blade.php
+++ resources/views/livewire/contest/award/add.blade.php
@@ -33,7 +33,7 @@ use App\Models\ContestSection;
                 <span class="fyk text-2xl">Awards</span>
             </a>
             . .
-            <a href="{{ route('contest-participant.modify', ['cid' => $contest->id ]); }}">
+            <a href="{{ route('contest-participant.modify', ['contest' => $contest]) }}">
                 <span class="fyk text-xl">Participants</span>
             </a>
             . .
