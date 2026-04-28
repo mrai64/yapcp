497084f6ab2c229cc568c139160842e4a220af07
mrai64 <maxrainato@libero.it>
2026-04-28 13:44:58 +0200

    feat: ContestAward policy

---
+++ app/Http/Controllers/ContestAwardController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreContestAwardRequest;
+use App\Http\Requests\UpdateContestAwardRequest;
+use App\Models\ContestAward;
+
+class ContestAwardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreContestAwardRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ContestAward $contestAward)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ContestAward $contestAward)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateContestAwardRequest $request, ContestAward $contestAward)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ContestAward $contestAward)
+    {
+        //
+    }
+}
