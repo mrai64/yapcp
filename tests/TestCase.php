00a64c17b1f886e323a172e5f8ac6543c199b476
mrai64 <maxrainato@libero.it>
2026-05-04 21:57:30 +0200

    feat: add federationMoreSeeders

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -50,6 +50,9 @@ abstract class TestCase extends BaseTestCase
         // federations
         $this->seed(\Database\Seeders\FederationSeeder::class);
 
+        // federations more fields
+        $this->seed(\Database\Seeders\FederationMoreSeeder::class);
+
         // organizations
         $this->seed(\Database\Seeders\OrganizationSeeder::class);
         // echo "\nTestCase done\n";
