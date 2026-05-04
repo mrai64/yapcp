1328ac79872b35f91f2121929908d8246c40d3eb
mrai64 <maxrainato@libero.it>
2026-05-04 10:18:25 +0200

    feat: ContestSection factory n Seeder

--- database/seeders/ContestSeeder.php
+++ database/seeders/ContestSeeder.php
@@ -2,12 +2,12 @@
 
 /**
  * Contest are build from organization
- * 
+ *
  */
+
 namespace Database\Seeders;
 
 use App\Models\Contest;
-use App\Models\Organization;
 use Illuminate\Database\Seeder;
 
 class ContestSeeder extends Seeder
