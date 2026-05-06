4fa3084439df5525d366ec966a5fe531dee33fcb
mrai64 <maxrainato@libero.it>
2026-05-06 14:02:26 +0200

    fix: missing UserContactMore*

---
+++ database/seeders/UserContactMoreSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserContactMoreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
