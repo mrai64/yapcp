107c3fdd5f857f5062c4d22ea976a7ee9f5688a2
mrai64 <maxrainato@libero.it>
2026-05-11 17:36:51 +0200

    fix: database add

--- database/migrations/2026_05_06_150432_add_idx_to_user_contact_mores.php
+++ database/migrations/2026_05_06_150432_add_idx_to_user_contact_mores.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('user_contact_mores', function (Blueprint $table) {
-            // 
+            //
             $table->index('user_id', 'user_idx');
         });
     }
