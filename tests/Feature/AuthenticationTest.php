64d4007367af5e1660850b721cbe93ab15976f7a
mrai64 <maxrainato@libero.it>
2026-05-27 07:59:31 +0200

    fix: Authentication

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -13,7 +13,7 @@ test('users can authenticate using the login screen', function () {
 
     $response = $this->post('/login', [
         'email' => $user->email,
-        'password' => 'password',
+        'password' => 'Password@12345', // was: password
     ]);
 
     $this->assertAuthenticated();
