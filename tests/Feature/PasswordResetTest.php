5bd4c1def6f14afec52dbd123de800d46dd13a96
mrai64 <maxrainato@libero.it>
2026-05-27 07:54:18 +0200

    fix: PasswordReset

--- tests/Feature/PasswordResetTest.php
+++ tests/Feature/PasswordResetTest.php
@@ -60,8 +60,8 @@ test('password can be reset with valid token', function () {
         $response = $this->post('/reset-password', [
             'token' => $notification->token,
             'email' => $user->email,
-            'password' => 'password',
-            'password_confirmation' => 'password',
+            'password' => 'Password@12345',
+            'password_confirmation' => 'Password@12345',
         ]);
 
         $response->assertSessionHasNoErrors();
