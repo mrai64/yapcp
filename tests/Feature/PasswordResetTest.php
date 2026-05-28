b2102e22cebe344b7768719b6ebead58e0e5a0a6
mrai64 <maxrainato@libero.it>
2026-05-28 15:11:55 +0200

    fix: PasswordResetTest

--- tests/Feature/PasswordResetTest.php
+++ tests/Feature/PasswordResetTest.php
@@ -3,6 +3,7 @@
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Fortify\Features;
 
 test('reset password link screen can be rendered', function () {
@@ -36,13 +37,12 @@ test('reset password screen can be rendered', function () {
         'email' => $user->email,
     ]);
 
-    Notification::assertSentTo($user, ResetPassword::class, function (object $notification) {
-        $response = $this->get('/reset-password/'.$notification->token);
+    Notification::assertSentTo($user, ResetPassword::class);
+    
+    $notification = Notification::sent($user, ResetPassword::class)->first();
+    $response = $this->get('/reset-password/'.$notification->token);
 
-        $response->assertStatus(200);
-
-        return true;
-    });
+    $response->assertStatus(200);
 })->skip(function () {
     return ! Features::enabled(Features::resetPasswords());
 }, 'Password updates are not enabled.');
@@ -56,18 +56,18 @@ test('password can be reset with valid token', function () {
         'email' => $user->email,
     ]);
 
-    Notification::assertSentTo($user, ResetPassword::class, function (object $notification) use ($user) {
-        $response = $this->post('/reset-password', [
-            'token' => $notification->token,
-            'email' => $user->email,
-            'password' => 'Password@12345',
-            'password_confirmation' => 'Password@12345',
-        ]);
+    Notification::assertSentTo($user, ResetPassword::class);
+    
+    $notification = Notification::sent($user, ResetPassword::class)->first();
+    $response = $this->post('/reset-password', [
+        'token' => $notification->token,
+        'email' => $user->email,
+        'password' => 'Password@12345',
+        'password_confirmation' => 'Password@12345',
+    ]);
 
-        $response->assertSessionHasNoErrors();
-
-        return true;
-    });
+    $response->assertSessionHasNoErrors();
+    expect(Hash::check('Password@12345', $user->fresh()->password))->toBeTrue();
 })->skip(function () {
     return ! Features::enabled(Features::resetPasswords());
 }, 'Password updates are not enabled.');
