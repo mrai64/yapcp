422e108647b7c91be712f69c95fac85fa8e76b39
mrai64 <maxrainato@libero.it>
2026-05-29 07:49:23 +0200

    fix: welcome [skip ci]

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,16 +2,16 @@
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                        <div class="flex lg:justify-start fyk text-xl ">yaPCP</div>
+                        <div class="flex lg:justify-start fyk text-2xl ">&nbsp;</div>
                         <div class="flex lg:justify-center lg:col-start-2">
                             <!-- yaPCP logo -->
-                            <x-application-logo class="h-12 w-auto text-white lg:h-16 lg:text-[#FF2D20]" width="60px" height="60px" />
+                            <x-application-logo class="size-20 lg:size-24 text-white lg:text-[#FF2D20]" />
                             <!--/yaPCP logo -->
                         </div>
                     </header>
 
                     <main class="mt-6 max-w-l mx-auto lg:mt-16 lg:max-w-4xl min-h-screen h-full">
-                        <p class="mb-4">Welcome to Photo Contest Platform, <br />
+                        <p class="mb-4">🇬🇧 Welcome to Photo Contest Platform, <br />
                             <em>our mission</em> is help <strong>you</strong> to have
                             a comfortable way to participate or organize Photo Contest everywhere you are.
                             <br />
@@ -48,14 +48,14 @@
                         </p>
 
                         @else
-                        <p class="h-10">&nbsp;</p>
+                        <p class="h-6">&nbsp;</p>
                         <p class="fyk text-2xl text-center my-6">
                             <a href="{{ route('login') }}">
                                 {{ __('Log in') }}
                             </a>
                         </p>
                             @if (Route::has('register'))
-                        <p class="h-10">&nbsp;</p>
+                        <p class="h-6">&nbsp;</p>
                         <p class="fyk text-2xl text-center my-6">
                             <a href="{{ route('register') }}">
                                 {{ __('Register') }}
@@ -64,14 +64,14 @@
                             @endif
                         @endauth
 
-                        <p class="h-10">&nbsp;</p>
+                        <p class="h-6">&nbsp;</p>
                         <p class="fyk text-2xl text-center my-6">
                             <a href="{{ route('contest.list') }}">
                                 {{ __('The Open Contest List') }}
                             </a>
                         </p>
 
-                        <p class="h-10">&nbsp;</p>
+                        <p class="h-6">&nbsp;</p>
                         <p class="fyk text-2xl text-center my-6">
                             <a href="{{ url('/docs') }}">
                                 {{ __('The Manual') }}
