8433e724021ca14cbad1439a775c0f982c786d16
mrai64 <maxrainato@libero.it>
2026-05-29 10:52:28 +0200

    fix: landing page lazarus

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>yaPCP</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=yanone-kaffeesatz:400" rel="stylesheet" />
+
+        <!-- Styles -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased font-sans">
+        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+                <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                        <div class="flex lg:justify-start fyk text-xl ">yaPCP</div>
+                        <div class="flex lg:justify-center lg:col-start-2">
+                            <!-- yaPCP logo -->
+                            <x-application-logo class="h-12 w-auto text-white lg:h-16 lg:text-[#FF2D20]" width="60px" height="60px" />
+                            <!--/yaPCP logo -->
+                        </div>
+                    </header>
+
+                    <main class="mt-6 max-w-l mx-auto lg:mt-16 lg:max-w-4xl min-h-screen h-full">
+                        <p class="mb-4">Welcome to Photo Contest Platform, <br />
+                            <em>our mission</em> is help <strong>you</strong> to have
+                            a comfortable way to participate or organize Photo Contest everywhere you are.
+                            <br />
+                            <br />
+                            <ul>
+                                <li>- As participant,<br /> 
+                                    you can search and subscribe photo contest organized by 
+                                    national or international photo organization, hosted by yaPCP.
+                                    <br /><em>But also</em> you can leave your works in place if you plan
+                                    to participate on more contests hosted by yaPCP, your infos
+                                    are required only at first time, and you can change every time you need.
+                                    Change your home after registration? No problem: your data is updated
+                                    in every report that contest organizer run, after your modify.
+                                    <br />
+                                    <br />
+                                </li>
+                                <li>- As photo contest organizer,<br /> 
+                                    you can plan and manage in yaPCP your own photo contest, 
+                                    following national and international photo 
+                                    organization rules with the help of yaPCP.
+                                    If you plan to propose your contest for a Federation
+                                    Patronage, we guide you step by step in a compliant path.
+                                    <br />
+                                    <br />
+                                </li>
+                            </ul>
+                        </p>
+
+                        @auth
+                        <p class="fyk text-2xl text-center my-6">
+                            <a href="{{ route('user.dashboard') }}">
+                                {{ __('Your Dashboard') }}
+                            </a>
+                        </p>
+
+                        @else
+                        <p class="h-10">&nbsp;</p>
+                        <p class="fyk text-2xl text-center my-6">
+                            <a href="{{ route('login') }}">
+                                {{ __('Log in') }}
+                            </a>
+                        </p>
+                            @if (Route::has('register'))
+                        <p class="h-10">&nbsp;</p>
+                        <p class="fyk text-2xl text-center my-6">
+                            <a href="{{ route('register') }}">
+                                {{ __('Register') }}
+                            </a>
+                        </p>
+                            @endif
+                        @endauth
+
+                        <p class="h-10">&nbsp;</p>
+                        <p class="fyk text-2xl text-center my-6">
+                            <a href="{{ route('contest.list') }}">
+                                {{ __('The Open Contest List') }}
+                            </a>
+                        </p>
+
+                        <p class="h-10">&nbsp;</p>
+                        <p class="fyk text-2xl text-center my-6">
+                            <a href="{{ url('/docs') }}">
+                                {{ __('The Manual') }}
+                            </a>
+                        </p>
+
+                    </main>
+
+                    <footer class="py-16 text-center text-sm text-black dark:text-white/70 text-muted">
+                        &copy; {{ date('Y')}} - {{ config('app.name') }} - version {{ $appVersion }} guest
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
