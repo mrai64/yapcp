db144d7f1dfb47423c47cfe3ef845df7ec2f96fa
mrai64 <maxrainato@libero.it>
2026-06-12 14:22:50 +0200

    fix: test w/ snake_case fields

--- resources/views/livewire/federation-section/modify.blade.php
+++ resources/views/livewire/federation-section/modify.blade.php
@@ -18,17 +18,17 @@ use function Livewire\Volt\rules;
 state([
     'federationSection',
     'federation', // Federation input
-    'sectionCode' => '', // default values in table
-    'sectionNameEn' => '',
+    'code' => '', // default values in table
+    'name_en' => '',
     'synopsis' => '',
-    'minWorks' => 0,
-    'maxWorks' => 4,
-    'shortSizeMax' => 1080,
-    'longSizeMax' => 1080,
-    'fileSizeMax' => 100000,
-    'monochromaticRequired' => false,
-    'rawRequired' => false,
-    'uniquePrize' => false,
+    'min_works' => 0,
+    'max_works' => 4,
+    'short_size_max' => 1080,
+    'long_size_max' => 1080,
+    'file_size_max' => 100000,
+    'monochromatic_required' => false,
+    'raw_required' => false,
+    'unique_prize' => false,
 ]);
 
 // to view
@@ -36,33 +36,33 @@ mount( function (FederationSection $federation_section) {
     Log::info('Component ' . __FILE__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
     $this->federationSection = $federation_section;
     $this->federation = $federation_section->federation;
-    $this->sectionCode = $federation_section->code;
-    $this->sectionNameEn = $federation_section->name_en;
+    $this->code = $federation_section->code;
+    $this->name_en = $federation_section->name_en;
     $this->synopsis = $federation_section->synopsis; 
-    $this->minWorks = $federation_section->min_works;
-    $this->maxWorks = $federation_section->max_works;
-    $this->shortSizeMax = $federation_section->short_size_max;
-    $this->longSizeMax = $federation_section->long_size_max;
-    $this->fileSizeMax = $federation_section->file_size_max;
-    $this->monochromaticRequired = $federation_section->monochromatic_required;
-    $this->rawRequired = $federation_section->raw_required;
-    $this->uniquePrize = $federation_section->unique_prize;
+    $this->min_works = $federation_section->min_works;
+    $this->max_works = $federation_section->max_works;
+    $this->short_size_max = $federation_section->short_size_max;
+    $this->long_size_max = $federation_section->long_size_max;
+    $this->file_size_max = $federation_section->file_size_max;
+    $this->monochromatic_required = $federation_section->monochromatic_required;
+    $this->raw_required = $federation_section->raw_required;
+    $this->unique_prize = $federation_section->unique_prize;
 });
 
 // to validate
 rules( function (){
     Log::info('Component ' . __FILE__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
     return [
-        'sectionNameEn' => 'required|string|min:3|max:255',
+        'name_en' => 'required|string|min:3|max:255',
         'synopsis' => 'required|string',
-        'minWorks' => 'required|integer|min:0|max:20',
-        'maxWorks' => 'required|integer|min:1|max:20',
-        'shortSizeMax' => 'required|integer|min:1080|max:2500',
-        'longSizeMax' => 'required|integer|min:1080|max:2500',
-        'fileSizeMax' => 'required|integer|min:100000|max:6000000',
-        'monochromaticRequired' => 'boolean', // maybe boolean:strict but... too much
-        'rawRequired' => 'boolean',
-        'uniquePrize' => 'boolean',
+        'min_works' => 'required|integer|min:0|max:20',
+        'max_works' => 'required|integer|min:1|max:20',
+        'short_size_max' => 'required|integer|min:1080|max:2500',
+        'long_size_max' => 'required|integer|min:1080|max:2500',
+        'file_size_max' => 'required|integer|min:100000|max:6000000',
+        'monochromatic_required' => 'boolean', 
+        'raw_required' => 'boolean',
+        'unique_prize' => 'boolean',
     ];
 
 });
@@ -75,19 +75,19 @@ $modifyFederationSection = function () {
         . ' validated:' . json_encode($validate));
     
     $this->federationSection->update([
-        'name_en' => $validate['sectionNameEn'],
+        'name_en' => $validate['name_en'],
         'local_lang' => 'en',
-        'local_name' => $validate['sectionNameEn'],
+        'local_name' => $validate['name_en'],
         'synopsis' => $validate['synopsis'],
         'file_formats' => 'jpg',
-        'min_works' => $validate['minWorks'],
-        'max_works' => $validate['maxWorks'],
-        'short_size_max' => $validate['shortSizeMax'],
-        'long_size_max' => $validate['longSizeMax'],
-        'file_size_max' => $validate['fileSizeMax'],
-        'monochromatic_required' => $validate['monochromaticRequired'],
-        'raw_required' => $validate['rawRequired'],
-        'unique_prize' => $validate['uniquePrize'],
+        'min_works' => $validate['min_works'],
+        'max_works' => $validate['max_works'],
+        'short_size_max' => $validate['short_size_max'],
+        'long_size_max' => $validate['long_size_max'],
+        'file_size_max' => $validate['file_size_max'],
+        'monochromatic_required' => $validate['monochromatic_required'],
+        'raw_required' => $validate['raw_required'],
+        'unique_prize' => $validate['unique_prize'],
     ]);
 
     Log::debug('Component ' . __FILE__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ 
@@ -127,16 +127,16 @@ $modifyFederationSection = function () {
 
                 <!-- federation-section code -->
                 <div class="mb-4">
-                    <x-input-label for="sectionCode" :value="__('Section Id, Code')" />
-                    <x-text-input wire:model="sectionCode" id="sectionCode" class="block mt-1 w-48" type="text" name="sectionCode" readonly disabled />
+                    <x-input-label for="code" :value="__('Section Id, Code')" />
+                    <x-text-input wire:model="code" id="code" class="block mt-1 w-48" type="text" name="code" readonly disabled />
                 </div>
                 <!--/federation-section code -->
 
                 <!-- federation-section english name -->
                 <div class="mb-4">
-                    <x-input-label for="sectionNameEn" :value="__('Section name')" />
-                    <x-text-input wire:model="sectionNameEn" id="sectionNameEn" class="block mt-1 w-full" type="text" name="sectionNameEn" required placeholder="{{ __('English name') }}"/>
-                    <x-input-error for="sectionNameEn" class="mt-2" />
+                    <x-input-label for="name_en" :value="__('Section name')" />
+                    <x-text-input wire:model="name_en" id="name_en" class="block mt-1 w-full" type="text" name="name_en" required placeholder="{{ __('English name') }}"/>
+                    <x-input-error for="name_en" class="mt-2" />
                 </div>
                 <!--/federation-section english name -->
 
@@ -151,71 +151,71 @@ $modifyFederationSection = function () {
 
                 <!-- federation-section min works number-->
                 <div class="mb-4">
-                    <x-input-label for="minWorks" :value="__('Minimum works number for Section - from 0 to 20')" />
-                    <x-text-input wire:model="minWorks" id="minWorks" class="block mt-1 w-48" type="number" name="minWorks" required placeholder="{{ __('Integer, from 0 to 20') }}" min="0" max="20" />
-                    <x-input-error for="minWorks" class="mt-2" />
+                    <x-input-label for="min_works" :value="__('Minimum works number for Section - from 0 to 20')" />
+                    <x-text-input wire:model="min_works" id="min_works" class="block mt-1 w-48" type="number" name="min_works" required placeholder="{{ __('Integer, from 0 to 20') }}" min="0" max="20" />
+                    <x-input-error for="min_works" class="mt-2" />
                 </div>
                 <!--/federation-section min works number-->
 
                 <!-- federation-section max works number-->
                 <div class="mb-4">
-                    <x-input-label for="maxWorks" :value="__('Maximum works number for Section - from 1 to 20')" />
-                    <x-text-input wire:model="maxWorks" id="maxWorks" class="block mt-1 w-48" type="number" name="maxWorks" required placeholder="{{ __('Integer, from 1 to 20') }}" min="0" max="20" />
-                    <x-input-error for="maxWorks" class="mt-2" />
+                    <x-input-label for="max_works" :value="__('Maximum works number for Section - from 1 to 20')" />
+                    <x-text-input wire:model="max_works" id="max_works" class="block mt-1 w-48" type="number" name="max_works" required placeholder="{{ __('Integer, from 1 to 20') }}" min="0" max="20" />
+                    <x-input-error for="max_works" class="mt-2" />
                 </div>
                 <!--/federation-section max works number-->
 
                 <!-- federation-section short side size px -->
                 <div class="mb-4">
-                    <x-input-label for="shortSizeMax" :value="__('Max size for shortest side - px')" />
-                    <x-text-input wire:model="shortSizeMax" id="shortSizeMax" class="block mt-1 w-48" type="number" name="shortSizeMax" required placeholder="{{ __('Integer, from 1 to 20') }}" min="1080" max="2500" />
-                    <x-input-error for="shortSizeMax" class="mt-2" />
+                    <x-input-label for="short_size_max" :value="__('Max size for shortest side - px')" />
+                    <x-text-input wire:model="short_size_max" id="short_size_max" class="block mt-1 w-48" type="number" name="short_size_max" required placeholder="{{ __('Integer, from 1 to 20') }}" min="1080" max="2500" />
+                    <x-input-error for="short_size_max" class="mt-2" />
                 </div>
                 <!-- federation-section short side size px -->
 
                 <!-- federation-section long side size px -->
                 <div class="mb-4">
-                    <x-input-label for="longSizeMax" :value="__('Max size for longest side - px')" />
-                    <x-text-input wire:model="longSizeMax" id="longSizeMax" class="block mt-1 w-48" type="number" name="longSizeMax" required placeholder="{{ __('Integer, from 1 to 20') }}" min="1080" max="2500" />
-                    <x-input-error for="longSizeMax" class="mt-2" />
+                    <x-input-label for="long_size_max" :value="__('Max size for longest side - px')" />
+                    <x-text-input wire:model="long_size_max" id="long_size_max" class="block mt-1 w-48" type="number" name="long_size_max" required placeholder="{{ __('Integer, from 1 to 20') }}" min="1080" max="2500" />
+                    <x-input-error for="long_size_max" class="mt-2" />
                 </div>
                 <!-- federation-section short side size px -->
 
                 <!-- federation-section file-size Bytes -->
                 <div class="mb-4">
-                    <x-input-label for="fileSizeMax" :value="__('Max size for file size - B')" />
-                    <x-text-input wire:model="fileSizeMax" id="fileSizeMax" class="block mt-1 w-48" type="number" name="fileSizeMax" required placeholder="{{ __('Integer, from 100000     to 6000000') }}" min="100000" max="6000000" />
-                    <x-input-error for="fileSizeMax" class="mt-2" />
+                    <x-input-label for="file_size_max" :value="__('Max size for file size - B')" />
+                    <x-text-input wire:model="file_size_max" id="file_size_max" class="block mt-1 w-48" type="number" name="file_size_max" required placeholder="{{ __('Integer, from 100000     to 6000000') }}" min="100000" max="6000000" />
+                    <x-input-error for="file_size_max" class="mt-2" />
                 </div>
                 <!--/federation-section file-size Bytes -->
 
                 <!-- federation-section Monochromatic Required On/Off -->
                 <div class="mb-4">
-                    <label for="monochromaticRequired" class="ms-2 text-sm text-gray-600">
-                        <x-checkbox wire:model="monochromaticRequired" id="monochromaticRequired" name="monochromaticRequired" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" />
+                    <label for="monochromatic_required" class="ms-2 text-sm text-gray-600">
+                        <x-checkbox wire:model="monochromatic_required" id="monochromatic_required" name="monochromatic_required" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" />
                         {{ __('Exclusively monochromatic images') }}
                     </label>
-                    <x-input-error for="monochromaticRequired" class="mt-2" />
+                    <x-input-error for="monochromatic_required" class="mt-2" />
                 </div>
                 <!-- federation-section Monochromatic Required On/Off -->
 
                 <!-- federation-section RAW maybe Required On/Off -->
                 <div class="mb-4">
-                    <label for="rawRequired" class="ms-2 text-sm text-gray-600">
-                        <x-checkbox wire:model="rawRequired" id="rawRequired" name="rawRequired" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" />
+                    <label for="raw_required" class="ms-2 text-sm text-gray-600">
+                        <x-checkbox wire:model="raw_required" id="raw_required" name="raw_required" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" />
                         {{ __('Original RAW should be required') }}
                     </label>
-                    <x-input-error for="rawRequired" class="mt-2" />
+                    <x-input-error for="raw_required" class="mt-2" />
                 </div>
                 <!--/federation-section RAW maybe Required On/Off -->
 
                 <!-- federation-section Cumulative prizes On/Off -->
                 <div class="mb-4">
-                    <label for="uniquePrize" class="ms-2 text-sm text-gray-600">
-                        <x-checkbox wire:model="uniquePrize" id="uniquePrize" name="uniquePrize" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" />
+                    <label for="unique_prize" class="ms-2 text-sm text-gray-600">
+                        <x-checkbox wire:model="unique_prize" id="unique_prize" name="unique_prize" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" />
                         {{ __('Only A prize for author in section') }}
                     </label>
-                    <x-input-error for="uniquePrize" class="mt-2" />
+                    <x-input-error for="unique_prize" class="mt-2" />
                 </div>
                 <!-- federation-section Cumulative prizes On/Off -->
 
