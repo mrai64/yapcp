ace6767f26199a1c6f75cdba60774ff12da9ec66
mrai64 <maxrainato@libero.it>
2026-06-15 21:41:17 +0200

    chore: message [skip ci]

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,18 +23,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        echo "\n  Create 25 users";
-        // $newOrUpdate = User::factory()->make([
-        //     'name' => 'Rainato, Massimo',
-        //     'email' => 'massimo.rainato@athesis77.it',
-        //     'password' => Hash::make('massimo.rainato@athesis77.it'),
-        //     'remember_token' => Str::random(10),
-        // ])->toArray();
-        // User::upsert($newOrUpdate, ['email'], []);
-        if (!User::whereEmail('massimo.rainato.3@athesis77.it')->exists()) {
+        echo "\n  Create 25 users, plus 1 founder admin";
+        if (!User::whereEmail('massimo.rainato@athesis77.it')->exists()) {
             User::create([
                 'name' => 'Rainato, Massimo',
-                'email' => 'massimo.rainato.3@athesis77.it',
+                'email' => 'massimo.rainato@athesis77.it',
                 'email_verified_at' => now(),
                 'password' => Hash::make('massimo.rainato@athesis77.it'),
                 'remember_token' => Str::random(10),
