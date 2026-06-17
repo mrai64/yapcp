be27cbd6162696acf58ab428d025da49c56e0987
mrai64 <maxrainato@libero.it>
2026-06-17 19:25:34 +0200

    chore: comment or empty fle removed

--- resources/views/livewire/user/contest/listed.blade.php
+++ resources/views/livewire/user/contest/listed.blade.php
@@ -96,7 +96,6 @@ new class extends Component {
                             <dd class="mt-2 text-gray-600 flex items-center">
                                 <svg class="w-5 h-5 mr-2 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                                 <span class="font-semibold mr-1">{{ __('Closing date') }}:</span> 
-                                {{-- Mostriamo la data convertita nella Timezone dell'utente per chiarezza --}}
                                 <span title="Local: {{ $contest->day_2_closing->setTimezone($contest->timezone_id ?? 'UTC')->format('Y-m-d H:i') }} ({{ $contest->timezone_id }})">
                                     {{ $contest->day_2_closing->setTimezone($userTz)->format('Y-m-d H:i') }}
                                 </span>
