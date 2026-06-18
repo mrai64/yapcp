e25ea6706069a64ded69f66b19691df3bd933b32
mrai64 <maxrainato@libero.it>
2026-06-18 15:49:48 +0200

    feat: profile can't change unused, initial name

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -52,12 +52,14 @@
             </div>
         @endif
 
-        <!-- Name -->
-        <div class="col-span-6 sm:col-span-4">
-            <x-label for="name" value="{{ __('Name') }}" />
-            <x-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" required autocomplete="name" />
-            <x-input-error for="name" class="mt-2" />
-        </div>
+        @php
+     //   <!-- Name -->
+     //   <div class="col-span-6 sm:col-span-4">
+     //       <x-label for="name" value="{{ __('Name') }}" />
+     //       <x-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" required autocomplete="name" />
+     //       <x-input-error for="name" class="mt-2" />
+     //   </div>
+        @endphp
 
         <!-- Email -->
         <div class="col-span-6 sm:col-span-4">
