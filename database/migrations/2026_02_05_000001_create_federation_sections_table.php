a2a97273a92af1037018ec0f1c2e16c2efdd6ab2
mrai64 <maxrainato@libero.it>
2026-07-06 09:44:30 +0200

    feat: rebuild / 59 migration fix federation_sections

--- database/migrations/2026_02_05_000001_create_federation_sections_table.php
+++ database/migrations/2026_02_05_000001_create_federation_sections_table.php
@@ -19,37 +19,45 @@ return new class () extends Migration {
     {
         Schema::create('federation_sections', function (Blueprint $table) {
             $table->id()->comment('real pk is federation_id + code');
-            $table->char('federation_id', 10)->charset('ascii')->collation('ascii_general_ci')->index();
-            $table->char('code', 10)->charset('ascii')->collation('ascii_general_ci')->index();
+            $table->char('federation_id', 10)
+                ->charset('ascii')->collation('ascii_general_ci')
+                ->index();
+            $table->char('code', 10)
+                ->charset('ascii')->collation('ascii_general_ci')
+                ->index();
 
             $table->string('name_en')->index()->comment('official name in english');
 
             $table->char('local_lang', 2)->default('en')->comment('follow iso-3166 2 ascii lowercase');
             $table->string('name_local')->default('')->comment('in local name');
 
-            $table->text('rule_definition')->nullable()->comment('synopsis from federal regulation docs');
+            $table->text('synopsis')->nullable()->comment('synopsis from federal regulation docs');
 
             // automatic check-able rules
             $table->string('file_formats')->default('jpg,tif,raw,raf,nef,cr2')->comment('list of ext, comma separated');
             $table->unsignedInteger('min_works')->default(0)->comment('greater zero == portfolio');
             $table->unsignedInteger('max_works')->default(4);
-            $table->unsignedInteger('min_short_side')->default(1080)->comment('px');
-            $table->unsignedInteger('max_long_side')->default(2500)->comment('px');
-            $table->integer('max_weight')->default(6000000)->comment('Bytes');
+            $table->unsignedInteger('short_size_max')->default(1080)->comment('px');
+            $table->unsignedInteger('long_size_max')->default(2500)->comment('px');
+            $table->unsignedInteger('file_size_max')->default(6000)->comment('KB');
 
-            $table->boolean('monochromatic_required')->default(false)->comment('0 == false, 1 == true');
+            $table->boolean('monochromatic_required')->default(false)
+                ->comment('0 == false, 1 == true');
             $table->boolean('raw_required')->default(false)
                 ->comment('section require raw original works (not only)');
-            $table->boolean('only_one')->default(false)
+            $table->boolean('unique_prize')->default(false)
                 ->comment('required only one prize / author n section');
 
             $table->dateTime('created_at')->useCurrent();
             $table->dateTime('updated_at')->useCurrentOnUpdate()->useCurrent()->index();
             $table->dateTime('deleted_at')->nullable()->index();
-
+            // idx
             $table->unique(['federation_id', 'code'], 'general_idx');
-
-            $table->foreign(['federation_id'])->references(['id'])->on('federations')->onUpdate('restrict')->onDelete('restrict');
+            // fk
+            $table->foreign(['federation_id'])
+                ->references(['id'])->on('federations')
+                ->onUpdate('restrict')
+                ->onDelete('restrict');
         });
     }
 
