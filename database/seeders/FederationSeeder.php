e2056e05c30d754f027e72717a751171d3bc3cc2
mrai64 <maxrainato@libero.it>
2026-07-26 15:13:11 +0200

    feat: FederationSeeder reusable

--- database/seeders/FederationSeeder.php
+++ database/seeders/FederationSeeder.php
@@ -22,31 +22,34 @@ class FederationSeeder extends Seeder
     {
         //
         Schema::disableForeignKeyConstraints();
-        Federation::truncate();
-        Schema::enableForeignKeyConstraints();
         //
-        Federation::factory()->create([
-            'id' => 'FIAP',
+        Federation::firstOrCreate(
+            [ 'id' => 'FIAP', ],
+            [
             'country_id' => 'LUX',
             'name_en' => "Fédération Internationale de l'Art Photographique",
             // local_lang
             // name_local
-            'timezone_id' => 'Europe/Rome',
+            'timezone_id' => 'Europe/Luxembourg',
             'website' => 'https://www.fiap.net/en/',
             'contact_info' => ' ',
-        ]);
-        Federation::factory()->create([
-            'id' => 'FIAF',
+        ]
+        );
+        Federation::firstOrCreate(
+            [ 'id' => 'FIAF', ],
+            [
             'country_id' => 'ITA',
             'name_en' => "Italian Photographic Society' Federation",
             'local_lang' => 'it',
             'name_local' => 'Federazione Italiana Associazioni Fotografiche',
             'timezone_id' => 'Europe/Rome',
             'website' => 'https://www.fiaf.net/',
-            'contact_info' => "corso San Martino, 8\n10122 Torino TO",
-        ]);
-        Federation::factory()->create([
-            'id' => 'UIF',
+            'contact_info' => "Via Vallaraita, 3\n10126 Torino TO",
+        ]
+        );
+        Federation::firstOrCreate(
+            [ 'id' => 'UIF', ],
+            [
             'country_id' => 'ITA',
             'name_en' => 'Italian Photoamateurs Union',
             'local_lang' => 'it',
@@ -54,45 +57,58 @@ class FederationSeeder extends Seeder
             'timezone_id' => 'Europe/Rome',
             'website' => 'https://www.unioneitalianafotoamatori.it/',
             'contact_info' => "Via Acque 9\n17045 Mallare SV",
-        ]);
-        Federation::factory()->create([
+        ]
+        );
+        Federation::firstOrCreate(
+            [ 'id' => 'GPU', ],
+            [
             'country_id' => 'GRC',
-            'id' => 'GPU',
             'name_en' => 'Global Photographic Union',
             'website' => 'https://www.gpuphoto.com/',
             'timezone_id' => 'Europe/Athens',
-            ]);
-        Federation::factory()->create([
+            ]
+        );
+        Federation::firstOrCreate(
+            [ 'id' => 'PAA', ],
+            [
             'country_id' => 'USA',
-            'id' => 'PAA',
             'name_en' => 'Photographic Alliance of America',
             'website' => 'https://www.paausa.org/',
             'timezone_id' => 'America/Chicago',
-        ]);
-        Federation::factory()->create([
+        ]
+        );
+        Federation::firstOrCreate(
+            [ 'id' => 'IAAP', ],
+            [
             'country_id' => 'MKD', // North Macedonia
-            'id' => 'IAAP',
             'name_en' => 'International Association of Art Photographers',
             'website' => 'https://theiaap.com/',
             'timezone_id' => 'Europe/Skopje',
             'contact_info' => 'e: officeiaap@gmail.com, t: +389 78240006',
-        ]);
-        Federation::factory()->create([
+        ]
+        );
+        Federation::firstOrCreate(
+            [ 'id' => 'RPS', ],
+            [
             'country_id' => 'GBR', // Great Britain - United Kingdom
-            'id' => 'RPS',
             'name_en' => 'Royal Photographic Society, The -',
             'website' => 'https://rps.org/',
             'timezone_id' => 'Europe/London',
             'contact_info' => 'HERE _ 470 Bath Road - Bristol - United Kingdom, t: +44 117 316 4450',
-        ]);
-        Federation::factory()->create([
+            ]
+        );
+        Federation::firstOrCreate(
+            [ 'id' => 'IIG', ],
+            [
             'country_id' => 'IND', // India
-            'id' => 'IIG',
             'name_en' => 'India International Group',
             'website' => 'https://indiainternationalgroup.com/',
             'timezone_id' => 'Asia/Kolkata', // formerly Asia/Calcutta
             'contact_info' => 'e: email@threedots.in, t: +91 880 116 6930',
-        ]);
-
+        ]
+        );
+        //
+        Schema::enableForeignKeyConstraints();
+        //
     }
 }
