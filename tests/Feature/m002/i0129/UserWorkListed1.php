d616fea5cfc6407cbf98a60633b6419940aceb39
mrai64 <maxrainato@libero.it>
2026-08-05 15:27:51 +0200

    feat: text list UserWork / basic test

---
+++ tests/Feature/m002/i0129/UserWorkListed1.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use App\Models\UserContact;
+use App\Models\UserWork;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Livewire\Volt\Volt;
+
+beforeEach(function () {
+    $this->user = User::factory()->create([
+        'name' => 'Rossi, Mario',
+        'email_verified_at' => now(),
+    ]);
+
+    $this->userContact = UserContact::firstOrCreate(
+        ['id' => $this->user->id],
+        [
+            'email' => $this->user->email,
+            'first_name' => 'Mario',
+            'last_name' => 'Rossi',
+            'country_id' => 'ITA',
+        ]
+    );
+});
+
+it('allows authenticated users with contact to access user.work.listed1 page', function () {
+    $this->actingAs($this->user)
+        ->get(route('user.work.listed1'))
+        ->assertSuccessful()
+        ->assertSeeLivewire('user.work.listed1');
+});
