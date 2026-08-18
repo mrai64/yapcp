a99174dfb0378043a27be3994ee9513e6f025928
mrai64 <maxrainato@libero.it>
2026-08-18 10:02:04 +0200

    feat: Federation / reset fk for every federation_id

---
+++ database/migrations/2026_08_18_091334_mod_fk_to_user_roles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_roles', function (Blueprint $table) {
+            // reset onUpdate onDelete to restrict
+            $table->dropForeign('federation_fk');
+            //
+            $table->foreign(['federation_id'], 'federation_fk')
+                ->references(['id'])->on('federations')
+                ->onUpdate('restrict')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_roles', function (Blueprint $table) {
+            // reset onUpdate onDelete to restrict
+            $table->dropForeign('federation_fk');
+            //
+            $table->foreign(['federation_id'], 'federation_fk')
+                ->references(['id'])->on('federations')
+                ->onUpdate('restrict')
+                ->onDelete('restrict');
+        });
+    }
+};
