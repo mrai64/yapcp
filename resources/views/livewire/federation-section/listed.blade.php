2740baf71c1ba69fe6076b418c16978ba11ab9f5
mrai64 <maxrainato@libero.it>
2026-08-20 17:43:28 +0200

    fix: FederationSection / Listed now differentiate for admin

--- resources/views/livewire/federation-section/listed.blade.php
+++ resources/views/livewire/federation-section/listed.blade.php
@@ -27,7 +27,7 @@ new class extends Component {
 			->where('federation_id', $this->federation->id)
 			->orderBy('code')
 			->get();
-		$this->isAdmin = Auth::user()->isAdmin();
+        $this->isAdmin = Auth::user()->isAdmin();
 	}
 
 }; ?>
@@ -54,7 +54,7 @@ new class extends Component {
 		<x-yapcp.header-link 
 			txt="Federation list" 
 			url="{{ route('federation.listed') }}" />
-			@if ($this->isAdmin)
+		@if ($this->isAdmin)
 		<x-yapcp.header-link 
 			txt="Add a Federation Section" 
 			url="{{ route('federation-section.add', ['federation' => $this->federation]) }}" />
