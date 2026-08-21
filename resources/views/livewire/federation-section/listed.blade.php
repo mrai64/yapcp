2de0195995478a214976dc4ba7668c9445a29eb4
mrai64 <maxrainato@libero.it>
2026-08-21 11:37:13 +0200

    fix: FederationSection / monochromatic_required field fix

--- resources/views/livewire/federation-section/listed.blade.php
+++ resources/views/livewire/federation-section/listed.blade.php
@@ -142,7 +142,7 @@ new class extends Component {
                 <dd class="px-5">
                     {{ $section->unique_prize ? '✅ YES' : '❌ NO' }}
                     | 
-                    {{ __('Not cumulable prizes in section') }} 
+                    {{ __('Not cumulabile prizes in section') }} 
                 </dd>
             </div>
             @endforeach
