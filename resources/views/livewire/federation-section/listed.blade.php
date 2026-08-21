2d2d42e80ba17303d4d90cb0b3947074f5f18057
mrai64 <maxrainato@libero.it>
2026-08-21 11:37:13 +0200

    fix: FederationSection / monochromatic_required field fix

--- resources/views/livewire/federation-section/listed.blade.php
+++ resources/views/livewire/federation-section/listed.blade.php
@@ -127,27 +127,27 @@ new class extends Component {
 										{{ __(':shortsize for short side, and :longsize for long side', ['shortsize' => $section->short_size_max, 'longsize' => $section->long_size_max, ] ) }}
 								</dd>
 
-                <dd class="px-5">
-                    {{ $section->monochrome_required ? '✅ YES' : '❌ NO' }}
-                    | 
-                    {{ __('Monochrome only') }}
-                </dd>
-
-                <dd class="px-5">
-                    {{ $section->raw_required ? '✅ YES' : '❌ NO' }}
-                    | 
-                    {{ __('RAW when asked') }}
-                </dd>
-
-                <dd class="px-5">
-                    {{ $section->unique_prize ? '✅ YES' : '❌ NO' }}
-                    | 
-                    {{ __('Not cumulable prizes in section') }} 
-                </dd>
-            </div>
-            @endforeach
-        </dl>
-        @else
+								<dd class="px-5">
+										{{ $section->monochromatic_required ? '✅ YES' : '❌ NO' }}
+										| 
+										{{ __('Monochrome only') }}
+								</dd>
+								
+								<dd class="px-5">
+										{{ $section->raw_required ? '✅ YES' : '❌ NO' }}
+										| 
+										{{ __('RAW when asked') }}
+								</dd>
+								
+								<dd class="px-5">
+										{{ $section->unique_prize ? '✅ YES' : '❌ NO' }}
+										| 
+										{{ __('Not cumulable prizes in section') }} 
+								</dd>
+						</div>
+						@endforeach
+				</dl>
+				@else
 				<p class="fyk font-semibold text-2xl text-gray-800 leading-tight fyk">
 					{{ $federation->name_en }}
 					<br />
