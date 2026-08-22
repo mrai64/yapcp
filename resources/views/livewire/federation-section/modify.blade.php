499f32f227034cb95c54b7edc686fccdfd8ee844
mrai64 <maxrainato@libero.it>
2026-08-22 18:51:20 +0200

    fix: FederationSection / Modify form review

--- resources/views/livewire/federation-section/modify.blade.php
+++ resources/views/livewire/federation-section/modify.blade.php
@@ -1,15 +1,16 @@
 <?php
 /**
  * Federation Section Modify
- * 
+ *
  * 2025-10-16 federations and federation_sections refactorize
  * 2026-06-10 refactor as volt-livewire 4
- * 
+ *
  */
 
 use App\Models\Federation;
 use App\Models\FederationSection;
 use Illuminate\Support\Facades\Log;
+
 use function Livewire\Volt\state;
 use function Livewire\Volt\mount;
 use function Livewire\Volt\rules;
@@ -32,13 +33,13 @@ state([
 ]);
 
 // to view
-mount( function (FederationSection $federation_section) {
+mount(function (FederationSection $federation_section) {
     Log::info('Component ' . __FILE__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
     $this->federationSection = $federation_section;
     $this->federation = $federation_section->federation;
     $this->code = $federation_section->code;
     $this->name_en = $federation_section->name_en;
-    $this->synopsis = $federation_section->synopsis; 
+    $this->synopsis = $federation_section->synopsis;
     $this->min_works = $federation_section->min_works;
     $this->max_works = $federation_section->max_works;
     $this->short_size_max = $federation_section->short_size_max;
@@ -50,17 +51,18 @@ mount( function (FederationSection $federation_section) {
 });
 
 // to validate
-rules( function (){
+rules(function () {
     Log::info('Component ' . __FILE__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
     return [
+        'code' => 'required|string|uppercase|min:2|max:10',
         'name_en' => 'required|string|min:3|max:255',
         'synopsis' => 'required|string',
         'min_works' => 'required|integer|min:0|max:20',
         'max_works' => 'required|integer|min:1|max:20',
         'short_size_max' => 'required|integer|min:1080|max:2500',
-        'long_size_max' => 'required|integer|min:1080|max:2500',
+        'long_size_max' => 'required|integer|min:1080|max:4000',
         'file_size_max' => 'required|integer|min:100000|max:6000000',
-        'monochromatic_required' => 'boolean', 
+        'monochromatic_required' => 'boolean',
         'raw_required' => 'boolean',
         'unique_prize' => 'boolean',
     ];
@@ -71,9 +73,9 @@ rules( function (){
 $modifyFederationSection = function () {
     Log::info('Component ' . __FILE__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ . ' called');
     $validate = $this->validate();
-    Log::debug('Component ' . __FILE__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ 
+    Log::debug('Component ' . __FILE__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__
         . ' validated:' . json_encode($validate));
-    
+
     $this->federationSection->update([
         'name_en' => $validate['name_en'],
         'local_lang' => 'en',
@@ -90,14 +92,14 @@ $modifyFederationSection = function () {
         'unique_prize' => $validate['unique_prize'],
     ]);
 
-    Log::debug('Component ' . __FILE__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__ 
-        . ' federationSection updated' );
+    Log::debug('Component ' . __FILE__ . ' f:' . __FUNCTION__ . ' l:' . __LINE__
+        . ' federationSection updated');
     // redirect
     return redirect()
         ->route('federation-section.listed', ['federation' => $this->federation])
         ->with('success', __('Federation section updated, well done!'));
 
-}; 
+};
 
 ?>
 
@@ -113,10 +115,15 @@ $modifyFederationSection = function () {
             {{ __("I.e. the federation card id si a federation-more field, when surname not, it´s a common field.") }}
         </p>
         <p class="fyk text-xl font-medium mb-4">
-            <a href="{{ route('federation-section.listed', ['federation' => $federationSection->federation_id] ) }}"
-                rel="noopener noreferrer">
-                [ {{ __("Back to Federation Section list") }} ]
-            </a>
+            <x-yapcp.header-link 
+                txt="Back to User dashboard" 
+                url="{{ route('user.dashboard') }}" />
+            <x-yapcp.header-link 
+                txt="Federation list" 
+                url="{{ route('federation.listed') }}" />
+            <x-yapcp.header-link 
+                txt="Federation Section list" 
+                url="{{ route('federation-section.listed', ['federation' => $federation] ) }}" />
         </p>
     </x-slot>
     <div class="py-12">
@@ -127,8 +134,12 @@ $modifyFederationSection = function () {
 
                 <!-- federation-section code -->
                 <div class="mb-4">
-                    <x-input-label for="code" :value="__('Section Id, Code')" />
-                    <x-text-input wire:model="code" id="code" class="block mt-1 w-48" type="text" name="code" readonly disabled />
+                    <p class="fyk text-2xl font-medium text-gray-900">
+                        {{ $code }}  
+                    </p>
+                    <p class="small">
+                        {{ __('Code is key, instead of change code you must insert a new record, then delete that.') }}
+                    </p>
                 </div>
                 <!--/federation-section code -->
 
@@ -142,17 +153,23 @@ $modifyFederationSection = function () {
 
 
                 <!-- federation-section synopsis -->
-                <div class="mb-4">
-                    <x-input-label for="synopsis" :value="__('Section definition, Synopsis')" />
-                    <x-text-input wire:model="synopsis" id="synopsis" class="block mt-1 w-full" type="text" name="synopsis" required placeholder="{{ __('From official docs, ev. translated in english text') }}" />
-                    <x-input-error for="synopsis" class="mt-2" />
-                </div>
+                    <div class="mb-4">
+                        <style>textarea {resize:vertical;}</style>
+                        <x-input-label for="synopsis" :value="__('Section definition, Synopsis')" />
+                        <textarea 
+                        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full" 
+                        type="text" name="synopsis"
+                        wire:model="synopsis"
+                        required placeholder="{{ __('From official docs, ev. translated in english text') }}"
+                        >{{ old('synopsis') }}</textarea>
+                        <x-input-error for="synopsis" class="mt-2" />
+                    </div>
                 <!--/federation-section synopsis -->
 
                 <!-- federation-section min works number-->
                 <div class="mb-4">
                     <x-input-label for="min_works" :value="__('Minimum works number for Section - from 0 to 20')" />
-                    <x-text-input wire:model="min_works" id="min_works" class="block mt-1 w-48" type="number" name="min_works" required placeholder="{{ __('Integer, from 0 to 20') }}" min="0" max="20" />
+                    <x-text-input wire:model="min_works" id="min_works" class="block mt-1 w-60" type="number" name="min_works" required placeholder="{{ __('Integer, from 0 to 20') }}" min="0" max="20" />
                     <x-input-error for="min_works" class="mt-2" />
                 </div>
                 <!--/federation-section min works number-->
@@ -160,31 +177,31 @@ $modifyFederationSection = function () {
                 <!-- federation-section max works number-->
                 <div class="mb-4">
                     <x-input-label for="max_works" :value="__('Maximum works number for Section - from 1 to 20')" />
-                    <x-text-input wire:model="max_works" id="max_works" class="block mt-1 w-48" type="number" name="max_works" required placeholder="{{ __('Integer, from 1 to 20') }}" min="0" max="20" />
+                    <x-text-input wire:model="max_works" id="max_works" class="block mt-1 w-60" type="number" name="max_works" required placeholder="{{ __('Integer, from 1 to 20') }}" min="1" max="20" />
                     <x-input-error for="max_works" class="mt-2" />
                 </div>
                 <!--/federation-section max works number-->
 
                 <!-- federation-section short side size px -->
                 <div class="mb-4">
-                    <x-input-label for="short_size_max" :value="__('Max size for shortest side - px')" />
-                    <x-text-input wire:model="short_size_max" id="short_size_max" class="block mt-1 w-48" type="number" name="short_size_max" required placeholder="{{ __('Integer, from 1 to 20') }}" min="1080" max="2500" />
+                    <x-input-label for="short_size_max" :value="__('Max size for shortest side - px from 1080 to 2500')" />
+                    <x-text-input wire:model="short_size_max" id="short_size_max" class="block mt-1 w-60" type="number" name="short_size_max" required placeholder="{{ __('Integer, from 1080 .. 2500') }}" min="1080" max="2500" />
                     <x-input-error for="short_size_max" class="mt-2" />
                 </div>
                 <!-- federation-section short side size px -->
 
                 <!-- federation-section long side size px -->
                 <div class="mb-4">
-                    <x-input-label for="long_size_max" :value="__('Max size for longest side - px')" />
-                    <x-text-input wire:model="long_size_max" id="long_size_max" class="block mt-1 w-48" type="number" name="long_size_max" required placeholder="{{ __('Integer, from 1 to 20') }}" min="1080" max="2500" />
+                    <x-input-label for="long_size_max" :value="__('Max size for longest side - px from 1080 to 4000')" />
+                    <x-text-input wire:model="long_size_max" id="long_size_max" class="block mt-1 w-60" type="number" name="long_size_max" required placeholder="{{ __('Integer, from 1080 .. 4000') }}" min="1080" max="4000" />
                     <x-input-error for="long_size_max" class="mt-2" />
                 </div>
                 <!-- federation-section short side size px -->
 
                 <!-- federation-section file-size Bytes -->
                 <div class="mb-4">
-                    <x-input-label for="file_size_max" :value="__('Max size for file size - B')" />
-                    <x-text-input wire:model="file_size_max" id="file_size_max" class="block mt-1 w-48" type="number" name="file_size_max" required placeholder="{{ __('Integer, from 100000     to 6000000') }}" min="100000" max="6000000" />
+                    <x-input-label for="file_size_max" :value="__('Max size for file size - B from 100000 to 6000000')" />
+                    <x-text-input wire:model="file_size_max" id="file_size_max" class="block mt-1 w-60" type="number" name="file_size_max" required placeholder="{{ __('Integer, from 100000 (for 100KB) .. 6000000 (for 6MB)') }}" min="100000" max="6000000" />
                     <x-input-error for="file_size_max" class="mt-2" />
                 </div>
                 <!--/federation-section file-size Bytes -->
@@ -193,7 +210,7 @@ $modifyFederationSection = function () {
                 <div class="mb-4">
                     <label for="monochromatic_required" class="ms-2 text-sm text-gray-600">
                         <x-checkbox wire:model="monochromatic_required" id="monochromatic_required" name="monochromatic_required" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" />
-                        {{ __('Exclusively monochromatic images') }}
+                        <span class="fyk text-xl font-medium ">{{ __('Exclusively monochromatic images') }}</span>
                     </label>
                     <x-input-error for="monochromatic_required" class="mt-2" />
                 </div>
@@ -203,7 +220,7 @@ $modifyFederationSection = function () {
                 <div class="mb-4">
                     <label for="raw_required" class="ms-2 text-sm text-gray-600">
                         <x-checkbox wire:model="raw_required" id="raw_required" name="raw_required" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" />
-                        {{ __('Original RAW should be required') }}
+                        <span class="fyk text-xl font-medium ">{{ __('Original RAW should be required') }}</span>
                     </label>
                     <x-input-error for="raw_required" class="mt-2" />
                 </div>
@@ -213,18 +230,17 @@ $modifyFederationSection = function () {
                 <div class="mb-4">
                     <label for="unique_prize" class="ms-2 text-sm text-gray-600">
                         <x-checkbox wire:model="unique_prize" id="unique_prize" name="unique_prize" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" />
-                        {{ __('Only A prize for author in section') }}
+                        <span class="fyk text-xl font-medium ">{{ __('One prize only, per author per section') }}</span>
                     </label>
                     <x-input-error for="unique_prize" class="mt-2" />
                 </div>
                 <!-- federation-section Cumulative prizes On/Off -->
 
-                <p>&nbsp;</p>
-                <button type="submit" 
-                    class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ms-3"
-                    >
-                    {{ __('Modify') }}
-                </button>
+                    <br style="clear:both;" />
+
+                    <x-button class="mt-2 ms-4">
+                        {{ __('Check all, then Modify') }}
+                    </x-button>
             </form>
         </div>
     </div>
