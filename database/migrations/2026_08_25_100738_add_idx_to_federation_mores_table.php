49199b88cdf60ede7e9561f4243693ff94a3d714
mrai64 <maxrainato@libero.it>
2026-08-25 10:09:43 +0200

    fix: FederationMore / idx for fk [skip ci]

---
+++ database/migrations/2026_08_25_100738_add_idx_to_federation_mores_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('federation_mores', function (Blueprint $table) {
+            $table->index(['federation_id'], 'federation_idx');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('federation_mores', function (Blueprint $table) {
+            $table->dropIndex('federation_idx');
+        });
+    }
+};
