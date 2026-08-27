a79c931262669ea17be26a320a8f5ffeaa84198f
mrai64 <maxrainato@libero.it>
2026-08-27 14:44:01 +0200

    feat: FederationMore / Modify test for pest (init)

--- database/factories/FederationMoreFactory.php
+++ database/factories/FederationMoreFactory.php
@@ -17,7 +17,13 @@ class FederationMoreFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'referenced' => fake()->text(40),
+            'federation_id' => fake()->regexify('[A-Z]{4}'),
+            'field_name' => fake()->text(20),
+            'field_label' => fake()->text(30),
+            'field_validation_rules' => 'string|max:255',
+            'field_default_value' => 'abcdefghijk',
+            'field_default_value' => fake()->text(80),
         ];
     }
 }
