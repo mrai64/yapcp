4c525f69409da6a97ad65a9ca95011bb0240b12f
mrai64 <maxrainato@libero.it>
2026-09-08 09:20:29 +0200

    feat: ContestJury / add field

---
+++ database/migrations/2026_09_08_085527_add_col_to_contest_juries_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contest_juries', function (Blueprint $table) {
+            $table->string('qualify')->default('')
+                ->after('is_president')
+                ->comment('president of... professional ...');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contest_juries', function (Blueprint $table) {
+            $table->removeColumn('qualify');
+        });
+    }
+};
